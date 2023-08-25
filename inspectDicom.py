62010eda344ee762696cc7a28dbd8d1e138838cf
Foster <nfoster@idxx4y9547q2g.lan>
2023-08-25 14:20:12 -0400

    inspectDicom now takes a dicom-file-path as a command line argument

--- inspectDicom.py
+++ inspectDicom.py
@@ -1,8 +1,17 @@
+import sys
 import pydicom
 
-# Load example DICOM file
-# dicom_file_path = './images/example-dicom/example-dicom.dcm'
-dicom_file_path = './images/mosaic-dicoms/grid0-20pct-black-pad.dcm'
-ds = pydicom.dcmread(dicom_file_path)
+# Check for correct usage
+if len(sys.argv) != 2:
+    print(f"Usage: python {sys.argv[0]} path/to/dicom_file.dcm")
+    sys.exit(1)
+
+# Read dicom_file_path from command-line argument
+dicom_file_path = sys.argv[1] 
+
+try:
+    ds = pydicom.dcmread(dicom_file_path)
+except:
+    ds = f"Could not read file at {dicom_file_path}"
 
 print(ds)
