76e9b24a93764cb5836fd7804f61b57195cde4d1
Foster <nfoster@idxx4y9547q2g.lan>
2023-08-25 11:09:22 -0400

    update load script to load all dicom files to local webserver

--- loadToDicomWebServer.py
+++ loadToDicomWebServer.py
@@ -2,14 +2,14 @@ import os
 import pydicom
 from dicomweb_client.api import DICOMwebClient
 
-dicom_dir = './images/mosaic-dicoms'
+dicom_dir = './images/dicom-images'
 
 server_url = 'http://localhost:8008/dcm4chee-arc/aets/DCM4CHEE/rs'
 client = DICOMwebClient(server_url)
 
 for filename in os.listdir(dicom_dir):
     dicom_filepath = os.path.join(dicom_dir, filename)
-    dataset = pydicom.dcmread(dicom_filepath, force=True)
+    dataset = pydicom.dcmread(dicom_filepath)
     response = client.store_instances([dataset])
     
     if response.status_code == 200:
