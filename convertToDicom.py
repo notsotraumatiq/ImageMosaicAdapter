c513a6c2839968fbfc13a76829335c5ec6525387
nfosterxx <nate-foster@idexx.com>
2023-09-14 15:07:33 -0400

    allow zoom capability for inputs of three resolution pngs

--- convertToDicom.py
+++ convertToDicom.py
@@ -23,8 +23,25 @@ output_dir = './images/dicom-trials/output'
 # Create ouput directory if it doesn't exist
 os.makedirs(output_dir, exist_ok=True)
 
+# The below information for the moment is being generated ONCE
+# for all files being converted -- which means they will all have
+# the same UIDs for the following fields.
+# Some of these fields will be provided for us in metadata:
+
+# Unique UID prefix
+prefix = '1.2.826.0.1.3680043.10.1286.' # Generated externally
+implementationClassUID = prefix + '1'
+
+# Create a random studyId of 7 alphanumeric digits
+# This will eventually be changed to reflect an actual studyId
+studyId = ''.join(random.choices(string.ascii_uppercase + string.digits, k=7))
+
+# Generate uuids
+studyInstanceUID = generate_uid(prefix=prefix)
+seriesInstanceUID = generate_uid(prefix=prefix)
+
 # Loop through input directory images
-for filename in os.listdir(input_dir):
+for idx, filename in enumerate(os.listdir(input_dir)):
     image_path = os.path.join(input_dir, filename)
 
     # If the input file is not a png image...
@@ -38,18 +55,8 @@ for filename in os.listdir(input_dir):
         print(f"Could not read png input file", error)
         sys.exit(1)
 
-    # Unique UID prefix
-    prefix = '1.2.826.0.1.3680043.10.1286.' # Generated externally
-    implementationClassUID = prefix + '1'
-
-    # Create a random studyId of 7 alphanumeric digits
-    # This will eventually be changed to reflect an actual studyId
-    studyId = ''.join(random.choices(string.ascii_uppercase + string.digits, k=7))
-
-    # Generate uuids
+    # Generate UIDs
     sopInstanceUID = generate_uid(prefix=prefix)
-    studyInstanceUID = generate_uid(prefix=prefix)
-    seriesInstanceUID = generate_uid(prefix=prefix)
     dimensionOrgUID = generate_uid(prefix=prefix)
 
     # File Metadata
@@ -67,6 +74,7 @@ for filename in os.listdir(input_dir):
                                         file_meta=file_meta, preamble=b"\0" * 128)
     ds.is_little_endian = True
     ds.is_implicit_VR = False
+
     
     ds.ImageType = ['DERIVED', 'PRIMARY', 'VOLUME', 'NONE']
     ds.SOPClassUID = '1.2.840.10008.5.1.4.1.1.77.1.6' # VL Whole Slide Microscopy Image Storage
@@ -78,7 +86,8 @@ for filename in os.listdir(input_dir):
     ds.StudyInstanceUID = studyInstanceUID
     ds.SeriesInstanceUID = seriesInstanceUID
     ds.StudyID = studyId
-    ds.SeriesNumber = None # ?
+    ds.SeriesNumber = 1 # ?
+    ds.InstanceNumber = idx
 
     # Dimension Data
     dimensionOrg = pydicom.dataset.Dataset()
@@ -104,7 +113,6 @@ for filename in os.listdir(input_dir):
     ds.DimensionOrganizationType = 'TILED_FULL'
 
     # Image Data
-    print(f"width: {png_image.width}, height: {png_image.height}")
     ds.Rows = png_image.height
     ds.Columns = png_image.width
     ds.SamplesPerPixel = 3
@@ -115,7 +123,9 @@ for filename in os.listdir(input_dir):
     ds.HighBit = 7
     ds.PixelRepresentation = 0
 
-    ds.ImageOrientationSlide = [0, -1, 0, -1, 0, 0] # necessary
+    # Rotates the Image 90 deg to the left...
+    # compensating for Slim's auto-rotation when it expects a slide image
+    ds.ImageOrientationSlide = [0, -1, 0, 1, 0, 0]
 
     # Optical Path Data
     illumTypeCode = pydicom.dataset.Dataset()
@@ -140,105 +150,45 @@ for filename in os.listdir(input_dir):
     ds.NumberOfOpticalPaths = 1
     ds.TotalPixelMatrixFocalPlanes = 1
 
-    np_image = np.array(png_image.getdata(), dtype=np.uint16)[:,:3]
-    # img_array = np.array(png_image)
-    # ds.PixelData = np_image.tobytes()
+    np_image = np.array(png_image.getdata(), dtype=np.uint8)[:,:3]
 
-    # For each image, create 3 separate resolutions:
-    resolutions = [
-        {
-            'name': 'highRes',
-            'numberOfFrames': 182,
-            'width_factor': 12,
-            'height_factor': 12,
-            'pixelSpacing': [0.0009706, 0.0009701]
-        },
-        {
-            'name': 'medRes',
-            'numberOfFrames': 49,
-            'width_factor': 6,
-            'height_factor': 6,
-            'pixelSpacing': [0.0019412, 0.0019403]
-        },
-        {
-            'name': 'lowRes',
-            'numberOfFrames': 9,
-            'width_factor': 3,
-            'height_factor': 3,
-            'pixelSpacing': [0.0038824, 0.0038806]
-        }
-    ]
-    for res in resolutions:
+    # Output path
+    base_name, extension = os.path.splitext(filename)
+    output_filename = f"{base_name}{suffix}.dicom"
+    output_path = os.path.join(output_dir, output_filename)
 
-        # Output path
-        base_name, extension = os.path.splitext(filename)
-        output_filename = f"{base_name}{suffix}_{res['name']}.dicom"
-        output_path = os.path.join(output_dir, output_filename)
+    ds.Rows = png_image.height
+    ds.Columns = png_image.width
+    ds.TotalPixelMatrixColumns = png_image.width
+    ds.TotalPixelMatrixRows = png_image.height
 
-        total_width = png_image.width * res['width_factor']
-        total_height = png_image.height * res['height_factor']
-        
-        numberOfFrames = res['numberOfFrames']
-        ds.NumberOfFrames = numberOfFrames
+    # pixel_data_list = split_image(png_image)
+    # ds.NumberOfFrames = len(pixel_data_list)
+    ds.NumberOfFrames = '1'
+    ds.PixelData = np_image.tobytes()
 
-        resized_img = np.resize(np_image, (total_height, total_width, 3))
-        pixel_data = resized_img.tobytes()
+    pixelMatrixOrigin = pydicom.dataset.Dataset()
+    pixelMatrixOrigin.XOffsetInSlideCoordinateSystem = '0.0'
+    pixelMatrixOrigin.YOffsetInSlideCoordinateSystem = '0.0'
+    ds.TotalPixelMatrixOriginSequence = pydicom.sequence.Sequence([pixelMatrixOrigin])
 
-        frame_width = png_image.width
-        frame_height = png_image.height
-        frame_pixel_size = len(pixel_data) // numberOfFrames
-        pixelSpacing = res['pixelSpacing']
+    pixelMeasures = pydicom.dataset.Dataset()
+    pixelMeasures.SliceThickness = '0.0'
+    pixelMeasures.PixelSpacing = [0.008, 0.008]
+    pmSequence = pydicom.sequence.Sequence([pixelMeasures])
 
-        ds.TotalPixelMatrixColumns = total_width
-        ds.TotalPixelMatrixRows = total_height
+    wsmImageFrameType = pydicom.dataset.Dataset()
+    wsmImageFrameType.FrameType = ['DERIVED', 'PRIMARY', 'VOLUME', 'NONE']
+    wsmiftSequence = pydicom.sequence.Sequence([wsmImageFrameType])
 
-        pixel_data_sequence = pydicom.Sequence()
+    sharedFuncGroup = pydicom.dataset.Dataset()
+    sharedFuncGroup.PixelMeasuresSequence = pmSequence
+    sharedFuncGroup.WholeSlideMicroscopyImageFrameTypeSequence = wsmiftSequence
+    sfgSequence = pydicom.sequence.Sequence([sharedFuncGroup])
 
-        # frame width is 5x5 image minus borders (6)
-        # frame width: (width - 6) / 5
+    ds.SharedFunctionalGroupsSequence = sfgSequence
 
-        for frame_number in range(numberOfFrames):
-            start_index = frame_number * frame_pixel_size
-            end_index = start_index + frame_pixel_size
-            frame_pixel_data = pixel_data[start_index:end_index]
+    # Save Dicom Image
+    ds.save_as(output_path, write_like_original=False)
 
-            frame_ds = pydicom.Dataset()
-            frame_ds.PixelData = frame_pixel_data
-            frame_ds.InstanceNumber = frame_number + 1
-
-            frame_ds.Rows = frame_height
-            frame_ds.Columns = frame_width
-
-        #     pixel_data_sequence.append(frame_ds)
-
-        # ds.PixelData = pixel_data_sequence
-
-        # ds.PixelData = np.tile(pixel_data, int(numberOfFrames))
-        ds.PixelData = np_image.tobytes()
-
-
-        pixelMatrixOrigin = pydicom.dataset.Dataset()
-        pixelMatrixOrigin.XOffsetInSlideCoordinateSystem = '0.0'
-        pixelMatrixOrigin.YOffsetInSlideCoordinateSystem = '0.0'
-        ds.TotalPixelMatrixOriginSequence = pydicom.sequence.Sequence([pixelMatrixOrigin])
-
-        pixelMeasures = pydicom.dataset.Dataset()
-        pixelMeasures.SliceThickness = '0.0'
-        pixelMeasures.PixelSpacing = pixelSpacing
-        pmSequence = pydicom.sequence.Sequence([pixelMeasures])
-
-        wsmImageFrameType = pydicom.dataset.Dataset()
-        wsmImageFrameType.FrameType = ['DERIVED', 'PRIMARY', 'VOLUME', 'NONE']
-        wsmiftSequence = pydicom.sequence.Sequence([wsmImageFrameType])
-
-        sharedFuncGroup = pydicom.dataset.Dataset()
-        sharedFuncGroup.PixelMeasuresSequence = pmSequence
-        sharedFuncGroup.WholeSlideMicroscopyImageFrameTypeSequence = wsmiftSequence
-        sfgSequence = pydicom.sequence.Sequence([sharedFuncGroup])
-
-        ds.SharedFunctionalGroupsSequence = sfgSequence
-
-        # Save Dicom Image
-        ds.save_as(output_path, write_like_original=False)
-
-        print(f"Modified version of {filename} stored as {output_path}")
+    print(f"Modified version of {filename} stored as {output_path}")
